ebd6a3d8c4e1933205e95a953af7c48f223d6be8
Avaz Bokiev <html5web@gmail.com>
2020-06-17 15:42:33 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,8 @@ const Home = () => (
   
            <h3>Click on the object to see it on AR mode. </h3>
         </a>
+  
+        <a href="intent://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;">Avocado</a>
 
       </div>
     </main>
