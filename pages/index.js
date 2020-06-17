de16868c00f3fdf430a2921b355b6c3b8893c695
Avaz Bokiev <html5web@gmail.com>
2020-06-17 18:18:11 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ const Home = () => (
       </h1>
 
       <div className="grid">
-        <a rel="ar" href="/chair_swan.usdz" className="card">
-           <img src="/redchair_2x.jpg" alt="AR file" />
+        <a rel="ar" href="/sag-carpet.usdz" className="card">
+           <img src="/sag-carpet.png" alt="AR file" />
   
            <h3>Click on the object to see it on AR mode. </h3>
         </a>
